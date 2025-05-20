f63f5cbada3d42827f252c224eed9a731206bcda
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-05-20 17:45:03 +0200

    Add new phase AQ_WAR
    new phase between PRE_AQ and AQ

--- src/IndividualProgression.h
+++ src/IndividualProgression.h
@@ -102,19 +102,20 @@ enum ProgressionState : uint8
     PROGRESSION_MOLTEN_CORE     = 1,
     PROGRESSION_ONYXIA          = 2,
     PROGRESSION_BLACKWING_LAIR  = 3,
-    PROGRESSION_PRE_AQ          = 4,
-    PROGRESSION_AQ              = 5,
-    PROGRESSION_NAXX40          = 6,
-    PROGRESSION_TBC_TIER_1      = 7,  // Karazhan, Gruul's Lair, Magtheridon's Lair
-    PROGRESSION_TBC_TIER_2      = 8,  // Serpentshrine Cavern, Tempest Keep
-    PROGRESSION_TBC_TIER_3      = 9,  // Hyjal Summit and Black Temple
-    PROGRESSION_TBC_TIER_4      = 10, // Zul'Aman
-    PROGRESSION_TBC_TIER_5      = 11, // Sunwell Plateau
-    PROGRESSION_WOTLK_TIER_1    = 12, // WotLK Naxx, EoE, OS
-    PROGRESSION_WOTLK_TIER_2    = 13, // Ulduar
-    PROGRESSION_WOTLK_TIER_3    = 14, // TotC
-    PROGRESSION_WOTLK_TIER_4    = 15, // ICC
-    PROGRESSION_WOTLK_TIER_5    = 16  // Ruby Sanctum
+    PROGRESSION_PRE_AQ          = 4, // AQ War effort, AQ quest line
+    PROGRESSION_AQ_WAR          = 5, // AQ outdoors war
+    PROGRESSION_AQ              = 6, // AQ gates open, raid available
+    PROGRESSION_NAXX40          = 7,
+    PROGRESSION_TBC_TIER_1      = 8,  // Karazhan, Gruul's Lair, Magtheridon's Lair
+    PROGRESSION_TBC_TIER_2      = 9,  // Serpentshrine Cavern, Tempest Keep
+    PROGRESSION_TBC_TIER_3      = 10,  // Hyjal Summit and Black Temple
+    PROGRESSION_TBC_TIER_4      = 11, // Zul'Aman
+    PROGRESSION_TBC_TIER_5      = 12, // Sunwell Plateau
+    PROGRESSION_WOTLK_TIER_1    = 13, // WotLK Naxx, EoE, OS
+    PROGRESSION_WOTLK_TIER_2    = 14, // Ulduar
+    PROGRESSION_WOTLK_TIER_3    = 15, // TotC
+    PROGRESSION_WOTLK_TIER_4    = 16, // ICC
+    PROGRESSION_WOTLK_TIER_5    = 17  // Ruby Sanctum
 };
 
 enum RandomDungeonIds : uint16
