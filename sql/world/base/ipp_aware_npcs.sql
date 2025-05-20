8b315507db1647a0ab53334843b8a4f2ade87717
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2025-05-21 01:58:25 +0200

    Update ipp_aware_npcs.sql

--- sql/world/base/ipp_aware_npcs.sql
+++ sql/world/base/ipp_aware_npcs.sql
@@ -15,7 +15,7 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_preaq' WHERE `entry` IN (
 UPDATE `creature` SET `ScriptName` = 'npc_ipp_preaq' WHERE `id1` = 15184 AND `guid` IN (42782, 42783, 42768);
 
 -- Phasing NPCs until after the outdoors AQ war has been completed
-UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_aq' WHERE `entry` IN  (16498, 15499, 15500, 15540, 16091);                 -- Cenarion Hold
+UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_aq' WHERE `entry` IN  (16498, 15499, 15500, 15540, 16091);                -- Cenarion Hold
 
 UPDATE `creature` SET `phaseMask` = @IPPPHASE_AQ WHERE `id1` IN (15612, 15613, 15615, 15616, 15617, 17070, 17079, 17766,         -- Orgrimmar Legion Post 
                                                                  15440, 15441, 15442, 15443, 15444, 15903, 17068, 17080, 17765); -- Ironforge Brigade post
