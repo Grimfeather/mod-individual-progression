f77764eb0ba9454ee1f4efb4d2870da61f73f438
unknown <mona@frennet.xyz>
2025-12-06 20:30:09 -0500

    SQL Insert file to update a few boss's that were missing Badge's of Justice. Mirrored to look like the other boss's who drop this item, min/max count is a bit of a guess.

---
+++ data/sql/world/updates/badges-of-justice.sql
@@ -0,0 +1,13 @@
+-- Tempest Keep bosses
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Chance`, `GroupId`, `MinCount`, `MaxCount`) VALUES
+(18805, 29434, 100, 1, 1, 1),  -- High Astromancer Solarian
+(19516, 29434, 100, 1, 1, 1),  -- Void Reaver
+(19622, 29434, 100, 1, 1, 2),  -- Kael'thas
+
+-- Serpentshrine Cavern bosses
+(21212, 29434, 100, 1, 1, 1),  -- Lady Vashj
+(21213, 29434, 100, 1, 1, 1),  -- Morogrim Tidewalker
+(21214, 29434, 100, 1, 1, 1),  -- Fathom-Lord Karathress
+(21215, 29434, 100, 1, 1, 1),  -- Leotheras the Blind
+(21216, 29434, 100, 1, 1, 1),  -- Hydross the Unstable
+(21217, 29434, 100, 1, 1, 1);  -- The Lurker Below
